e1c621e43ad51a15c11a5ff4d99b406c006f65d8
pakdad-mousavi <pakdad.mousavi@gmail.com>
2026-06-11 18:23:36 +0800

    Create the scanner module.

---
+++ packages/parser/src/scanner.ts
@@ -0,0 +1,53 @@
+import fs from "node:fs/promises";
+import path from "node:path";
+import ignore from "ignore";
+
+const isSupportedFileType = (filePath: string) => {
+  const supportedExtensions = [".js", ".ts"];
+  return supportedExtensions.includes(path.extname(filePath));
+};
+
+/**
+ *
+ * @param rootPath The path to the root directory containing the repository.
+ * @returns An array of filepaths (after being filtered out by any .gitignore files).
+ */
+export const scanRepo = async (rootPath: string) => {
+  // Ensure dir exists
+  try {
+    await fs.access(rootPath, fs.constants.F_OK);
+  } catch (e) {
+    throw new Error("Directory is not visible or cannot be found.");
+  }
+
+  //
+  try {
+    const filePaths = [];
+    const ig = ignore();
+
+    const dirs = await fs.readdir(rootPath, {
+      recursive: true,
+      withFileTypes: true,
+    });
+    for (const dir of dirs) {
+      // Only keep files
+      if (dir.isDirectory()) continue;
+
+      const filePath = path.join(dir.parentPath, dir.name);
+
+      const isIgnore = dir.name.includes(".gitignore");
+      if (isIgnore) {
+        const gitIgnoreContent = (await fs.readFile(filePath)).toString();
+        ig.add(gitIgnoreContent);
+        continue;
+      }
+
+      filePaths.push(path.relative(rootPath, filePath));
+    }
+
+    return ig.filter(filePaths).filter(isSupportedFileType);
+  } catch (e) {
+    console.log(e);
+    return [];
+  }
+};
