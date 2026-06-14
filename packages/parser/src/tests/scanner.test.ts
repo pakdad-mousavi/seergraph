a3e35766d25fe730f2d4c7e4dd33e9d124d304bb
pakdad-mousavi <pakdad.mousavi@gmail.com>
2026-06-14 15:56:57 +0800

    Write tests for scanner module.

---
+++ packages/parser/src/tests/scanner.test.ts
@@ -0,0 +1,17 @@
+import { describe, expect, test } from "vitest";
+import { scanRepo } from "../scanner";
+
+describe("scanRepo scans repository files correctly", () => {
+  test("ScanRepo scans the correct files", async () => {
+    const filepaths = await scanRepo("./src/dummy-files");
+
+    expect(filepaths.length).toBe(4);
+    expect(filepaths).toStrictEqual(["ts/auth.ts", "ts/index.ts", "ts/utils.ts", "ts/someFolder/test.ts"]);
+  });
+
+  test("ScanRepo throws an error when given a non-existent directory", async () => {
+    const filepaths = scanRepo("./fvfiwponpewnveqonvdon");
+
+    await expect(filepaths).rejects.toThrow("Directory is not visible or cannot be found.");
+  });
+});
