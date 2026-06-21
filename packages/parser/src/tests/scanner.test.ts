dbcf8b7bf0627c814acc95007e4b5a2ec5eb62b8
pakdad-mousavi <pakdad.mousavi@gmail.com>
2026-06-21 11:58:57 +0800

    Update scanner test file to include new dummy files.

--- packages/parser/src/tests/scanner.test.ts
+++ packages/parser/src/tests/scanner.test.ts
@@ -5,8 +5,14 @@ describe("scanRepo scans repository files correctly", () => {
   test("ScanRepo scans the correct files", async () => {
     const filepaths = await scanRepo("./src/dummy-files");
 
-    expect(filepaths.length).toBe(4);
-    expect(filepaths).toStrictEqual(["ts/auth.ts", "ts/index.ts", "ts/utils.ts", "ts/someFolder/test.ts"]);
+    expect(filepaths.length).toBe(5);
+    expect(filepaths).toStrictEqual([
+      "ts/auth.ts",
+      "ts/index.ts",
+      "ts/utils.ts",
+      "ts/someFolder/speak.ts",
+      "ts/someFolder/test.ts",
+    ]);
   });
 
   test("ScanRepo throws an error when given a non-existent directory", async () => {
